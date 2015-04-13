9a537d7c06e79714aeb081c44bf1b2b3cee183cf
Audun <audun@kinesthesiac.com>
2015-04-13 15:49:09 -0400

    OLP is now extensible

--- olp.py
+++ olp.py
@@ -1,16 +1,62 @@
-import sys, os, itertools
+import sys, os, itertools, re
+from OLPExtensions import *
+
 def get_command():
-    return " ".join([str(arg) for arg in sys.argv][1:])
+    '''
+    Stringify and return the command or definition given to OLP
+    '''
+
+    return " ".join([str(arg) for arg in sys.argv][1:]).strip()
+
+def define(some_input, force = False):
+    '''
+    Defines some variable or function for later use in OLP.
+    This is made availabe through the import of OLPExtensions
+    the next time the script is called.
+    Will not overwrite existing definitions unless force is set
+    to True
+    '''
+
+    definition_regex = re.compile('^((.)+=(.)+)+')
+    if re.match(definition_regex, some_input):
+        with open("/usr/local/bin/OLPExtensions.py", "wr") as definitions:
+            definitions.write(some_input + '\n')
+    else:
+        print("Not a valid definition")
+
+def run_command():   
+    '''
+    Eval and stringify the command, then return the result.
+    This function does not write to stdout.
+    '''
+
+    command = get_command()
+
+    try:
+        return str(eval(command))
+    except SyntaxError:
+        if command.strip() == "":
+            return "OLP : No command given"
+        else:
+            return "OLP : There was a syntax error in your command!"
+
+def run_command_from_pipe():
+    '''
+    Takes a set of values passed in from stdin, running the command on
+    each element, and yields the results
+    '''
+
+    stdin = sys.stdin
+    
 
 def main():
-    try : 
-        print(eval(get_command()))
-    except SyntaxError:
-        if get_command().strip() == "":
-            #We presume the call was an error and just pass the error silently
-            pass
-        else:
-            print("There was a syntax error in your command, please check for typos!")
+    command = get_command()
+    if command[0:2] == "-d":
+        define(command[3:])
+    else:
+        result = run_command()
+        if result:
+            print(result)
 
 if __name__ == "__main__":
     main()
