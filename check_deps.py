eff3115bc199bfb3f64160b95f4e2286eb151036
Audun <audun@kinesthesiac.com>
2015-04-17 23:11:48 -0400

    removed more refs

--- check_deps.py
+++
@@ -1,6 +0,0 @@
-import os
-def check_deps(deps):
-    for dep in deps:
-        if dep not in os.listdir('.'):
-            return False
-    return True
--- setup.sh
+++
@@ -1,8 +0,0 @@
-chmod +x olp
-cp olp /usr/local/bin
-cp olp.py /usr/local/bin
-cp OLPExtensions.py /usr/local/bin
-cp OLPModules.py /usr/local/bin
-cp OLPExceptions.py /usr/local/bin
-olp "'Done! :D'"
-
