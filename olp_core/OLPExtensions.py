2ec1094cffa67142609d3e5208a946ac9ec96dec
Audun <audun@kinesthesiac.com>
2015-04-19 05:42:16 -0400

    made olp.py exectuable without sh, OLP now prints full errors to user

---
+++ olp_core/OLPExtensions.py
@@ -0,0 +1,6 @@
+from OLPModules import *
+#This top line is necessary to keep OLP from name-erroring during eval.
+#System imports are kept in a seperate file like that to make
+#sure the entire OLP system has access to the same modules.
+#Below here you may have your fun :)
+
--- olp_core/olp.py
+++ olp_core/olp.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 try :
     from OLPExtensions import *
 except SyntaxError:
-    print("OLP : There is a syntax error in the OLPExtensions. OLPExtensions will be ignored")
+    print("OLP : There is a syntax error in the OLPExtensions. OLPExtensions \
+           will be ignored")
 from OLPExceptions import *
 from OLPModules import *
 
@@ -9,8 +11,21 @@ extensions_location = "/usr/local/bin/OLPExtensions.py"
 modules_location = "/usr/local/bin/OLPModules.py"
 
 def stdin_reader():
+    '''
+    Reads from stdin and yields every line as a string
+    '''
     for line in sys.stdin.read().split('\n'):
         yield line
+
+def set_placeholder_value(value, string_function):
+    '''
+    Used in conjuction with stdin_reader().
+
+    The idea here is that we are passed a value to map to _, which exists in
+    either the parameters or body of string_function. Then we return the
+    ammended stringified function
+    '''
+    return string.replace(string_function, '_', value)
 
 def get_command():
     '''
@@ -27,7 +42,8 @@ def add_module(module_name):
     if re.match(module_regex, module_name.strip()):
         with open(modules_location, "a") as modules:
             if module_name in globals():
-                print("OLP : Name " + module_name + " is already occupied in the global namespace")
+                print("OLP : Name " + module_name + " is already occupied in \
+                the global namespace")
                 print("OLP : Ignoring import attempt")
             else:
                 modules.write("import " + module_name + '\n')
@@ -61,28 +77,25 @@ def edit():
     '''
     os.system("/usr/bin/vi " + extensions_location)
 
-def run_command():   
+def run_command():
     '''
     Eval and stringify the command, then return the result.
     This function does not write to stdout.
     '''
 
     command = get_command()
-
-    try:
+    if command.strip() == "":
+        print("OLP: No command given")
+    else:
         return str(eval(command))
-    except SyntaxError:
-        if command.strip() == "":
-            return "OLP : No command given"
-        else:
-            return "OLP : There was a syntax error in your command!"
 
 def main():
     command = get_command()
-    command_params_regex = re.compile("^(-(df|d|i))+(.*)$")
+    command_params_regex = re.compile("^(-(df|d|i|e))+(.*)$")
     match = re.match(command_params_regex, command)
     if match:
-        param = match.group(1)
+        # If this block executes, OLP is either being fed a new definition or is importing a new module
+        param = match.group(1).lower()
         if param == '-df':
             print("Forced define")
             define(match.group(3).strip(), force = True)
@@ -90,9 +103,12 @@ def main():
             define(match.group(3).strip())
         elif param == "-i":
             add_module(match.group(3).strip())
+        elif param == "-e":
+            edit()
         else:
-                print("OLP : Unrecognised parameter")
+            print("OLP : Unrecognised parameter")
     else:
+        # OLP was passed no new definition or import, so we assume it's a regular command.
         result = run_command()
         if result:
             print(result)
