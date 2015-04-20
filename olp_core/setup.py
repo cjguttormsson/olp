28c5ee2cdba8ca0b3ba9778a83da2c642b3cdc04
Audun <audun@kinesthesiac.com>
2015-04-20 19:31:06 -0400

    added meta-data to setup.py

--- olp_core/setup.py
+++ olp_core/setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 setup(name = 'OLP',
       version = '0.1',
+      url = 'http://audun-.github.io/olp',
+      author = 'Audun B. Mo, @_audun_',
+      author_email = "audun@kinesthesiac.com",
       py_modules = ['OLPExtensions', 'OLPExceptions', 'OLPModules'],
       scripts = ['olp']
       )
