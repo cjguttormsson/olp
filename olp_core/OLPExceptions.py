ae909b7d2162c45000beecc9d6b8c028db97e4fb
Audun <audun@kinesthesiac.com>
2015-04-20 18:39:13 -0400

    slightly improved error catching, still needs work

--- olp_core/OLPExceptions.py
+++ olp_core/OLPExceptions.py
@@ -2,12 +2,32 @@ class OLPErrorTemplate(Exception):
     def __init__(self, msg):
         self.msg = msg
     def __str__(self):
-        return repr(self.msg)
+        return repr("OLP : " + self.msg)
 
 class OLPNameTaken(OLPErrorTemplate):
     def __init__(self, name):
         self.msg = "Name " + name + " is already occupied in the global namespace"
 
+class OLPNeedOverrideToDefine(OLPErrorTemplate):
+    def __init__(self, definition_name):
+        self.msg = "Name " + defintion_name + " is occupied in the global namespace, but you can override its definition by using the -df paramter"
 
-        
+class OLPNoCommand(OLPErrorTemplate):
+    def __init__(self):
+        self.msg = "No command given"
 
+class OLPInaccessibleModule(OLPErrorTemplate):
+    def __init__(self, module_name):
+        self.msg = "Module " + module_name + " is inaccessible"
+
+class OLPUnrecognisedParameter(OLPErrorTemplate):
+    def __init__(self, parameter):
+        self.msg = "Unrecognised paramter " + paramter
+
+class OLPInvalidDefintion(OLPErrorTemplate):
+    def __init__(self, definition):
+        self.msg = "Invalid definition: " + definition
+
+class OLPInvalidModuleName(OLPErrorTemplate):
+    def __init__(self, module_name):
+        self.msg = "Invalid module name: " + module_name
