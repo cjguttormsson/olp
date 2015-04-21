966df18438e7fa1827ed1d8167d11836f3b962e0
Audun <audun@kinesthesiac.com>
2015-04-20 20:28:38 -0400

    skeleton of unitests

---
+++ olp_core/tests.py
@@ -0,0 +1,32 @@
+import unittest
+
+class OLPInputTest(unittest.TestCase):
+    def test_definition_failure(self):
+        pass
+
+    def test_add_module_failure(self):
+        pass
+
+    def test_reference_defintion_failure(self):
+        pass
+
+    def test_reference_module_failure(self):
+        pass
+
+    def test_definition_success(self):
+        pass
+
+    def test_add_module_success(self):
+        pass
+
+    def test_reference_definition_success(self):
+        pass
+
+    def test_reference_module_success(self):
+        pass
+    
+    def test_stdin_pipe_failure(self):
+        pass
+
+    def test_stdin_pipe_success(self):
+        pass
